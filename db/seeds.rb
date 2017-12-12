076a8ea87975b145967fcbe8a3d63e34ae3a51aa
alexpahom <alex.pahomchik@gmail.com>
2017-12-12 13:34:20 +0300

    create and seed users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Users (username, first name, last name, email, phone)
+[
+  %w(Captain Steeve Rojers captaina@mar.com 111-222-333),
+  %w(Hulk Bruce Benner bennerb@mar.com 111-222-111),
+  %w(Iron Tony Stark starkt@ind.com 37533-304-33),
+  %w(EagleEye Clint Barton bartonc@mar.com 37533-550-23),
+  %w(Widow Natasha Romanova romann@mar.com 37529-302-33),
+  %w(Spider Peter Parker net@mar.com 37525-223-23)
+].each do |user, first, last, mail, phone_number|
+  user = User.create(
+    username: user,
+    first_name: first,
+    last_name: last,
+    email: mail,
+    phone: phone_number
+  )
+end
