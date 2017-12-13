32ddb483eb1626277d94302e2ce891070431f06a
alexpahom <alex.pahomchik@gmail.com>
2017-12-13 14:02:34 +0300

    modify validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # has_secure_password
-  # validates :email, presence: true, uniqueness: true, format: { with: /A.+@.+z/ }
-  # validates :username, presence: true, uniqueness: true
-  # validates :first_name, :last_name, presence: true
+  email_regex = /[a-zA-Z0-9_.+\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-.]+/
+  validates :email, presence: true, uniqueness: true, format: { with: email_regex }
+  validates :username, presence: true, uniqueness: true
+  validates :first_name, :last_name, presence: true
 end
