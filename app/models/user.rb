082734375776a441a65bca2ec94c2fd801161545
alexpahom <alex.pahomchik@gmail.com>
2018-01-08 16:04:50 +0300

    migration file

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
 
   email_regex = /[a-zA-Z0-9_.+\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-.]+/
   validates :email, presence: true, format: { with: email_regex },
-            length: { maximum: 20 }, uniqueness: { case_sensitive: false }
+            length: { maximum: 40 }, uniqueness: { case_sensitive: false }
   validates :username, presence: true, uniqueness: true, length: { maximum: 20 }
   validates :first_name, :last_name, presence: true, length: { maximum: 20 }
   validates :password, presence: true, length: { minimum: 6 }
