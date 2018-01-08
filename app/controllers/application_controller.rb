c1ceafca070e9f6e73b9d9b134a5c591a331434c
alexpahom <alex.pahomchik@gmail.com>
2018-01-08 16:32:49 +0300

    fix super admin issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
   before_action :require_permission
 
   def require_permission
+    return if current_user.role == 'master'
     redirect_back(fallback_location: root_path, alert: 'I bet you\'re not allowed to') if
         (controller_name == 'messages' && current_user.id != params[:user_id].to_i) ||
             (controller_name == 'users' && current_user.id != params[:id].to_i)
